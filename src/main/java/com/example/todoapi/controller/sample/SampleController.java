b131f41f5f848f6f3d6a7341489ddd19835454a3
mories-dev <mories1223@gmail.com>
2022-10-25 08:37:16 +0900

    todo-api追加

---
+++ src/main/java/com/example/todoapi/controller/sample/SampleController.java
@@ -0,0 +1,24 @@
+package com.example.todoapi.controller.sample;
+
+import com.example.todoapi.service.sample.SampleEntity;
+import com.example.todoapi.service.sample.SampleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/samples")
+@RequiredArgsConstructor
+public class SampleController {
+
+    private final SampleService service;
+
+    @GetMapping
+    public SampleDto index() {
+        var entity = service.find();
+        return new SampleDto(entity.getContent(), LocalDateTime.now());
+    }
+}
